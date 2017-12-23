5bd663c840752c09ef57546bf5d7697aa9a91e86
Robert Johansson <hellorobjo@gmail.com>
2017-12-23 21:29:28 +0100

    Fix packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='netstring_bin',
+    name='pynetstring',
     version='0.1.dev1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='A module for encoding and decoding netstrings',
-    long_description=open('README.txt').read(),
+    description='A module for encoding and decoding netstrings.',
+    long_description=open('README.rst').read(),
     install_requires=[],
-    url='https://github.com/rj79/netstring',
+    url='https://github.com/rj79/pynetstring',
     author='Robert Johansson',
     author_email='robertrockar@live.com'
 )
